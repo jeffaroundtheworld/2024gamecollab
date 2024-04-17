69082c544db7c41a1bbfcc99ecd162e4cb4f4589
Kod <159079366+kodriq@users.noreply.github.com>
2024-04-17 13:57:34 -0700

    coding for dialogue
    did some coding

---
+++ dialogue box.gd
@@ -0,0 +1,23 @@
+extends Control
+
+var dialogue = [
+'yo, im testing out this dialogue yo, this is crazy yo',
+'woah, me too bro, thats crazy bro. What the hell bro',
+'i can\'t do this dawg, HELP ME IM STUUUUUUUUUCK!!!!'
+]
+
+var dialogue_list = 0
+var finished = false
+
+
+func _ready():
+	play_dialoue()
+
+func play_dialogue():
+	if dialogie_list < dialogue.size(:)
+
+
+
+
+func _process(delta):
+	pass
