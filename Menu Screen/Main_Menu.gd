33d33a8f57617e42cbf610ed339e0d894436bff5
ediehouston <159079419+ediehouston@users.noreply.github.com>
2024-05-07 21:40:48 -0700

    Adding the video

--- Menu Screen/Main_Menu.gd
+++ Menu Screen/Main_Menu.gd
@@ -4,7 +4,7 @@ func _ready():
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Level design/school.tscn")
+	get_tree().change_scene_to_file("res://Cutscene/introcutscene.tscn")
 
 
 
