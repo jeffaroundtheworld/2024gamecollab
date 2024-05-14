109b70f9237be67395e04b5ff88a9faea0cc52ae
Kod <159079366+kodriq@users.noreply.github.com>
2024-05-14 15:08:00 -0700

    merged into main
    merged my branch into main and now im adding the friend to hospital level 3 so she can trigger the dialogue

--- friend.gd
+++ friend.gd
@@ -1,15 +1,15 @@
 extends Area3D
 
-var cutscene = preload("res://dialogue_testing.tscn")
+var cutscene = preload("res://dialogue scene/dialogue_scene.tscn")
 
 
 
 func _ready():
-	cutscene.instantiate()
-
+	pass
 
 
 
 func _on_body_entered(body):
 	if body.name == "player":
-		print ("bruh")
+		var newcutscene = cutscene.instantiate()
+		add_child(newcutscene)
