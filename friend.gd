e190cd7e13e702d8ed4e84ba40e07f460a5c584e
Kod <159079366+kodriq@users.noreply.github.com>
2024-05-16 12:12:31 -0700

    dialogue scene + trigger etc done
    all coding relating to the dialogue scene is finished (hopefully)

--- friend.gd
+++ friend.gd
@@ -11,10 +11,14 @@ func _ready():
 
 func _on_body_entered(body):
 	if body.name == "Player":
+		get_tree().paused = true
 		var newcutscene = cutscene.instantiate()
 		add_child(newcutscene)
 		$CollisionShape3D.queue_free()
 	
 
 func _on_friendalert_finished():
-	 $friendalert.play()
+	if get_node("CollisionShape3D") != null:
+		$friendalert.play()
+	else:
+		$friendalert.queue_free()
