a551542556d216e8b82a69a9d3e50af177a51a7c
Kod <159079366+kodriq@users.noreply.github.com>
2024-05-23 15:02:58 -0700

    added keycard
    doing the whole keycard thing SOMEBODY PLZ CODE THE WAY TO GO BACK THROUGH DOORS PLEAAAAAAAAAAAAAAAAAAAAAAAASE

--- Level design/Code/Area3D_part_2.gd
+++ Level design/Code/Area3D_part_2.gd
@@ -1,10 +1,10 @@
 extends Area3D
 func _on_body_entered(body):
 	if body.name == "Player":
-		get_tree().change_scene_to_file("res://Menu Screen/win_screen.tscn")
-		#if Globals.ending > 0:
-			#get_tree().change_scene_to_file("res://Menu Screen/ending_1.tscn")
-		#elif Globals.ending == 0:
-			#get_tree().change_scene_to_file("res://Menu Screen/ending_2.tscn")
+		#get_tree().change_scene_to_file("res://Menu Screen/win_screen.tscn")
+		if Globals.ending > 1:
+			get_tree().change_scene_to_file("res://Menu Screen/ending_1.tscn")
+		elif Globals.ending < 2:
+			get_tree().change_scene_to_file("res://Menu Screen/ending_2.tscn")
 
 			#^^^^^^^different ending code, but the scenes arent done 
