d3e33b3d4593d1ef4a9241fef5bb0dc76943bdff
ediehouston <159079419+ediehouston@users.noreply.github.com>
2024-05-27 13:04:39 -0700

    Control screen
    A screen that shows all the basic game controls

--- Menu Screen/Main_Menu.gd
+++ Menu Screen/Main_Menu.gd
@@ -11,5 +11,14 @@ func _on_play_pressed():
 
 
 
+
+
+func _on_controls_pressed():
+	get_tree().change_scene_to_file("res://Menu Screen/options_screen.tscn")
+	
+	
+	
+
+
 func _on_quit_pressed():
 	get_tree().quit()
