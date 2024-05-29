35fcd2d01bc603529cf1cf2ee8c65cb0dfc319d0
Kod <159079366+kodriq@users.noreply.github.com>
2024-05-29 13:51:47 -0700

    friend sound
    friend now says thank u for saving her

--- Level design/school_accessories/keycard.gd
+++ Level design/school_accessories/keycard.gd
@@ -3,7 +3,8 @@ extends Area3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if Globals.ending == 1:
+		queue_free()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
