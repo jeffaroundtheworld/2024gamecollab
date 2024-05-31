ee0174fd1fbc86b137ca2055d324238b02461808
Dylanimal <dylan.lockyer@shaw.ca>
2024-05-31 10:45:50 -0700

    Bug Fix

--- Menu Screen/Main_Menu.gd
+++ Menu Screen/Main_Menu.gd
@@ -2,7 +2,10 @@ extends Control
 
 func _ready():
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-	pass
+	Globals.ending = 0
+	Globals.respawn = 0
+	Globals.talked = 0 
+	Globals.cough = 0
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Cutscenes/introcutscene.tscn")
 
