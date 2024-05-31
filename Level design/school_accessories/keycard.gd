0d4fb887282351be372441a71cd62faf3011af3a
teachermanjeff <91495999+teachermanjeff@users.noreply.github.com>
2024-05-31 12:42:17 -0700

    Fixing Laird's merge screw up

--- Level design/school_accessories/keycard.gd
+++ Level design/school_accessories/keycard.gd
@@ -3,7 +3,8 @@ extends Area3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if Globals.ending == 1:
+		queue_free()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,4 +16,9 @@ func _on_body_entered(body):
 	if body.name == "Player":
 		Globals.ending += 1
 		print(Globals.ending)
-		queue_free()
+		$keycardgrab.play()
+		
+
+
+func _on_keycardgrab_finished():
+	queue_free()
--- globals.gd
+++ globals.gd
@@ -1,7 +1,5 @@
 extends Node
 
-
-#ending == 2 - you saved the friend, ending == 0 or 1 you did not
 var ending = 0
 var respawn
 var talked 
