3b1cacea06d642610e07cecf91a8a4edbacb78d6
Dylanimal <dylan.lockyer@shaw.ca>
2024-06-03 10:00:21 -0700

    Lose screen

--- Level design/Code/Area3D_part_2.gd
+++ Level design/Code/Area3D_part_2.gd
@@ -3,8 +3,8 @@ func _on_body_entered(body):
 	if body.name == "Player":
 		#get_tree().change_scene_to_file("res://Menu Screen/win_screen.tscn")
 		if Globals.ending > 1:
-			get_tree().change_scene_to_file("res://Menu Screen/ending_1.tscn")
+			get_tree().change_scene_to_file("res://Menu Screen/win_screen.tscn")
 		elif Globals.ending < 2:
-			get_tree().change_scene_to_file("res://Menu Screen/ending_2.tscn")
+			get_tree().change_scene_to_file("res://Menu Screen/ending_1.tscn")
 
 			#^^^^^^^different ending code, but the scenes arent done 
--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -1,24 +1,34 @@
 extends CharacterBody3D
+@onready var player = get_parent().get_parent().get_parent().get_node("Player")
+@onready var spawner = get_parent()
+@export var move_speed = 4
 
-@onready var animated_sprite_3d = $AnimatedSprite3D
-@export var move_speed = 1.5
-@export var attack_range = 2
+var dir_x
+var dir_z
+var dir_y
+const attack_range = 2
+const hide_range = 15
 
-var player
-var dead = false 
-
-
-func _ready():
-	var player = get_node("/root/Node3D/Player")
 func _physics_process(delta):
-	if dead : 
-		return
-	
 	var dir = player.global_position - global_position
 	var dir_y = 0
 	dir = dir.normalized()
+	dir_x = abs(abs(player.global_position.x) - abs(global_position.x))
+	dir_z = abs(abs(player.global_position.z) - abs(global_position.z))
+	dir_y = abs(abs(player.global_position.y) - abs(global_position.y))
 	
 	velocity = dir * move_speed
+
+	
+	if (dir_x<attack_range) and (dir_z<attack_range) and (dir_y<0.5):
+		player.attack = 1
+	else:
+		player.attack = 0
+	
+	if (dir_x>hide_range) or (dir_z>hide_range) or (dir_y>hide_range):
+		queue_free()
+		spawner.count = 0
+	
 	move_and_slide()
 	
 	
